489b025e9227c484869a687f2f718ae42aa8fd6a
VinterYoung5 <343196469@qq.com>
2024-12-23 19:01:55 +0800

    qp/bitrate spinner

--- app/src/main/java/com/GraphicsImageDecoderTest/DecoderTest/graphicsImageDecoder.java
+++ app/src/main/java/com/GraphicsImageDecoderTest/DecoderTest/graphicsImageDecoder.java
@@ -16,7 +16,11 @@ import android.provider.OpenableColumns;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,35 +40,50 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class graphicsImageDecoder extends AppCompatActivity
 {
 
     public static final int size = 20;
 
-    Button btnFile,btnEncode,btnPlay,btnPause;
-    TextView textView;
-    SurfaceView sfView_nor,sfView_roi,sfView_yuv;
+    public Button btnFile,btnEncode,btnPlay;
+    public Spinner spQP,spBR;
+    public ArrayAdapter<CharSequence> adapterQP = null;
+    public ArrayAdapter<CharSequence> adapterBR = null;
+    public TextView textView;
+    public SurfaceView sfView_nor,sfView_roi,sfView_yuv;
     static final String TAG = "roi_encoder";
     Uri uri = null;
 
     boolean isRoiSupport = false;
+    int qp = -6;
+    int bitrate = 20;
 
     File fileYUV = null;
     String fileName = null;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnFile = findViewById(R.id.btn_file);
         btnEncode = findViewById(R.id.btn_encode);
-        btnPlay = findViewById(R.id.btn_play);
-        btnPause = findViewById(R.id.btn_pause);
+        //btnPlay = findViewById(R.id.btn_play);
         btnEncode.setEnabled(false);
-        btnPlay.setEnabled(false);
-        btnPause.setEnabled(false);
+        //btnPlay.setEnabled(false);
+
+        spQP = findViewById(R.id.spinner_qp);
+        String[] nameQP = {"-6qp", "-5qp", "-4qp", "-3qp", "-2qp", "-1qp", "0qp",
+                "1qp", "2qp", "3qp", "4qp", "5qp", "6qp"};
+        adapterQP = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nameQP);
+        spQP.setAdapter(adapterQP);
+
+        spBR = findViewById(R.id.spinner_br);
+        String[] nameBR = {"50Mbps", "20Mbps", "10Mbps", "5Mbps", "2Mbps"};
+        adapterBR =  new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, nameBR);
+        spBR.setAdapter(adapterBR);
 
         textView = findViewById(R.id.textView);
 
@@ -75,9 +94,7 @@ public class graphicsImageDecoder extends AppCompatActivity
 
         btnFile.setOnClickListener(view -> {
             // check condition
-            if (!Environment.isExternalStorageManager())
-            {
-
+            if (!Environment.isExternalStorageManager()) {
                 Intent intent = new Intent(
                         Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
                 intent.setData(Uri.parse("package:" + graphicsImageDecoder.this.getPackageName()));
@@ -94,6 +111,40 @@ public class graphicsImageDecoder extends AppCompatActivity
             Run run = new Run();
             Thread thd = new Thread(run);
             thd.start();
+        });
+
+        spBR.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectBR = parent.getItemAtPosition(position).toString();
+                String regEx="[^-?0-9]";
+                Pattern p = Pattern.compile(regEx);
+                Matcher m = p.matcher(selectBR);
+                bitrate = Integer.parseInt(m.replaceAll("").trim());
+                Log.d(TAG, "spinner select bt " + selectBR+" br "+ bitrate);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spQP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectQP = parent.getItemAtPosition(position).toString();
+                String regEx="[^-?0-9]";
+                Pattern p = Pattern.compile(regEx);
+                Matcher m = p.matcher(selectQP);
+                qp = Integer.parseInt(m.replaceAll("").trim());
+                Log.d(TAG, "spinner select qp " + selectQP+" qp "+ qp);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
         });
     }
 
@@ -194,6 +245,7 @@ public class graphicsImageDecoder extends AppCompatActivity
             final int[] videoTrackid = {-1};
             final boolean[] inputEos = {false};
             final boolean[] outputEos = {false};
+            recIndex = 0;
             MediaCodec encoder;
             try {
                 encoder = MediaCodec.createEncoderByType(mMediaType);
@@ -212,11 +264,11 @@ public class graphicsImageDecoder extends AppCompatActivity
             String brm_s = bitRateMode == MediaCodecInfo.EncoderCapabilities.BITRATE_MODE_CBR ? "cbr" :
                     bitRateMode == MediaCodecInfo.EncoderCapabilities.BITRATE_MODE_VBR ? "vbr" : "cq";
             format.setInteger(MediaFormat.KEY_BITRATE_MODE, bitRateMode);
-            int bitRate = 20;
-            format.setInteger(MediaFormat.KEY_BIT_RATE, bitRate * 1024 * 1024);
+            //bitrate = 20;
+            format.setInteger(MediaFormat.KEY_BIT_RATE, bitrate * 1000 * 1000);
             int frameRate = 30;
             format.setInteger(MediaFormat.KEY_FRAME_RATE, frameRate);
-            int iFrameInterval = 0;
+            int iFrameInterval = 1;
             format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, iFrameInterval);
             // turn on ROI feature at initialized stage
             if (!useQcomRoi) {
@@ -233,8 +285,7 @@ public class graphicsImageDecoder extends AppCompatActivity
             }
             SimpleDateFormat form = new SimpleDateFormat("yyyyMMdd_HHmmss");
             Date date = new Date(System.currentTimeMillis());
-            int[] qp = {-6,6};
-            String name = fileName+"."+brm_s+"."+bitRate+"Mbps"+"."+frameRate+"fps"+"."+"qp("+qp[0]+"_"+qp[1]+")"+form.format(date);
+            String name = fileName+"."+brm_s+"."+bitrate+"Mbps"+"."+frameRate+"fps"+"."+"qp("+qp+")"+form.format(date);
             String outPath = "/storage/emulated/0/DCIM/"+name+".mp4";
             MediaMuxer mMediaMuxer;
 
@@ -273,13 +324,19 @@ public class graphicsImageDecoder extends AppCompatActivity
                     int flag = !inputEos[0] ? 0 : MediaCodec.BUFFER_FLAG_END_OF_STREAM;
 
                     Bundle param = new Bundle();
-                    String roIRects = "736,32-1280,256="+qp[0]+";736,256-1280,480="+qp[1]+";";
+                    //String roiRectsInfo = "736,32-1280,256="+qp+";736,256-1280,480=6;";
+                    //Rectangle 1 is top: 128, left: 96, bottom: 336, right: 272, delta QP: -3 and
+                    //Rectangle 2 is top: 128, left 1000, bottom: 440, right: 1100, delta QP: -4
+                    int index = recIndex >= recLocate.length ? recLocate.length-1 : recIndex;
+                    recIndex++;
+                    String roiRectsInfo = recLocate[index]+"="+qp+";";
+                    Log.d(TAG,"roirectinfo "+roiRectsInfo);
                     if (!useQcomRoi) {
-                        param.putString(MediaCodec.PARAMETER_KEY_QP_OFFSET_RECTS, roIRects);
+                        param.putString(MediaCodec.PARAMETER_KEY_QP_OFFSET_RECTS, roiRectsInfo);
                     } else {
                         //param.putLong("vendor.qti-ext-enc-roiinfo.timestamp",0);
                         param.putString("vendor.qti-ext-enc-roiinfo.type","rect");
-                        param.putString("vendor.qti-ext-enc-roiinfo.rect-payload",roIRects);
+                        param.putString("vendor.qti-ext-enc-roiinfo.rect-payload",roiRectsInfo);
                     }
                     encoder.setParameters(param);
                     queueCnt[0]++;
@@ -344,4 +401,98 @@ public class graphicsImageDecoder extends AppCompatActivity
 
         }
     }
+    public int recIndex = 0;
+    public static String[] recLocate = {
+            "688,0-1328,464",
+            "688,0-1344,464",
+            "688,0-1344,464",
+            "688,0-1344,464",
+            "688,0-1344,464",
+            "688,16-1344,464",
+            "688,16-1344,480",
+            "688,16-1344,480",
+            "688,16-1344,480",
+            "688,16-1344,480",
+            "688,16-1344,480",
+            "688,16-1344,480",
+            "688,16-1344,480",
+            "688,32-1344,480",
+            "688,32-1344,480",
+            "688,32-1344,480",
+            "688,32-1344,496",
+            "688,48-1344,496",
+            "688,48-1344,496",
+            "688,48-1344,512",
+            "688,64-1344,512",
+            "688,64-1344,512",
+            "688,64-1344,528",
+            "688,80-1344,528",
+            "688,80-1344,544",
+            "688,96-1344,544",
+            "688,96-1344,544",
+            "688,96-1344,560",
+            "688,96-1344,560",
+            "688,112-1344,560",
+            "688,112-1344,576",
+            "688,128-1344,576",
+            "688,128-1344,576",
+            "672,128-1344,592",
+            "672,128-1328,592",
+            "672,144-1328,592",
+            "672,144-1328,592",
+            "656,144-1328,592",
+            "656,144-1344,608",
+            "656,144-1344,608",
+            "656,144-1344,608",
+            "656,144-1344,608",
+            "656,160-1344,608",
+            "656,160-1344,624",
+            "656,160-1344,624",
+            "672,176-1344,640",
+            "672,176-1344,640",
+            "672,176-1344,640",
+            "672,192-1344,656",
+            "672,192-1344,656",
+            "672,192-1344,656",
+            "672,192-1344,656",
+            "672,192-1344,656",
+            "656,192-1328,656",
+            "656,208-1328,672",
+            "656,208-1328,672",
+            "656,224-1328,672",
+            "656,224-1328,688",
+            "656,240-1328,704",
+            "656,240-1328,704",
+            "656,256-1328,720",
+            "656,272-1328,736",
+            "656,288-1328,752",
+            "656,304-1344,768",
+            "656,336-1344,800",
+            "656,352-1344,816",
+            "656,384-1360,848",
+            "656,400-1360,880",
+            "656,432-1360,912",
+            "656,448-1376,944",
+            "656,480-1376,976",
+            "656,512-1376,1008",
+            "640,528-1392,1040",
+            "624,560-1392,1072",
+            "624,592-1392,1080",
+            "624,608-1392,1080",
+            "608,624-1392,1080",
+            "592,656-1392,1080",
+            "592,672-1392,1080",
+            "576,688-1392,1080",
+            "560,704-1392,1080",
+            "544,720-1392,1080",
+            "544,720-1392,1080",
+            "528,736-1392,1080",
+            "512,752-1392,1080",
+            "496,768-1392,1080",
+            "496,768-1392,1080",
+            "480,784-1392,1080",
+            "480,800-1360,1080",
+            "464,816-1360,1080",
+            "464,816-1360,1080",
+    };
 }
